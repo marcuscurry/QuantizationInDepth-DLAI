f9e0f2cb7ebfdd758a3372a22c55c9e3322e71fc
Marcus Curry <60270079+marcuscurry@users.noreply.github.com>
2024-06-20 13:04:26 -0500

    Add files via upload
    Init commit

---
+++ helper.py
@@ -0,0 +1,62 @@
+# helper.py
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def plot_matrix(tensor, ax, title, vmin=0, vmax=1, cmap=None):
+    """
+    Plot a heatmap of tensors using seaborn
+    """
+    sns.heatmap(tensor.cpu().numpy(), ax=ax, vmin=vmin, vmax=vmax, cmap=cmap, annot=True, fmt=".2f", cbar=False)
+    ax.set_title(title)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+
+
+def plot_quantization_errors(original_tensor, quantized_tensor, dequantized_tensor, dtype = torch.int8, n_bits = 8):
+    """
+    A method that plots 4 matrices, the original tensor, the quantized tensor
+    the de-quantized tensor and the error tensor.
+    """
+    # Get a figure of 4 plots
+    fig, axes = plt.subplots(1, 4, figsize=(15, 4))
+
+    # Plot the first matrix
+    plot_matrix(original_tensor, axes[0], 'Original Tensor', cmap=ListedColormap(['white']))
+
+    # Get the quantization range and plot the quantized tensor
+    q_min, q_max = torch.iinfo(dtype).min, torch.iinfo(dtype).max
+    plot_matrix(quantized_tensor, axes[1], f'{n_bits}-bit Linear Quantized Tensor', vmin=q_min, vmax=q_max, cmap='coolwarm')
+
+    # Plot the de-quantized tensors
+    plot_matrix(dequantized_tensor, axes[2], 'Dequantized Tensor', cmap='coolwarm')
+
+    # Get the quantization errors
+    q_error_tensor = abs(original_tensor - dequantized_tensor)
+    plot_matrix(q_error_tensor, axes[3], 'Quantization Error Tensor', cmap=ListedColormap(['white']))
+
+    fig.tight_layout()
+    plt.show()
+
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+def plot_results(model, pil_img, results):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    scores, labels, boxes = results["scores"], results["labels"], results["boxes"]
+    colors = COLORS * 100
+    for score, label, (xmin, ymin, xmax, ymax),c  in zip(scores.tolist(), labels.tolist(), boxes.tolist(), colors):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        text = f'{model.config.id2label[label]}: {score:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
